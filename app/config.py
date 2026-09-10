e1eb18490ca51d636867b41085803575b1ff632a
KaranMajila <majilakaran@gmail.comm>
2026-09-10 11:54:25 +0530

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class Settings:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_CLUSTER_ENDPOINT = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    GOOGLE_GEMINI_KEY = os.getenv("GOOGLE_GEMINI_KEY")
+    QDRANT_COLLECTION = "enterprise_rag"
+    GROQ_MODEL = "llama-3.3-70b-versatile"
+
+
+settings = Settings()
